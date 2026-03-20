7970ea8b598586aca4fc996970446b1097a1507d
WeiShenL <wslo@kth.se>
2026-03-20 12:13:17 +0100

    chore: scaffold react native project
    updated with scaffold structure + gitignore

---
+++ src/app/index.jsx
@@ -0,0 +1,5 @@
+// entry page
+import { Text } from "react-native"
+  
+// TODO pass reactive model down to presenters
+export default function IndexPage() { return <Text>Hello React Native!</Text> }

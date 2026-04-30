f4d128cf7dde0b6631a33ad06a396df8fcc61680
rameesha <rameesha@kth.se>
2026-04-30 17:35:30 +0200

    Update index.jsx

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { View } from "react-native";
 import { observer } from "mobx-react-lite";
 import { model } from "../model.js";
 
@@ -7,10 +7,8 @@ import HomePresenter from "../presenters/homePresenter.jsx";
 
 export default observer(function ExplorePage() {
   return (
-    <ScrollView>
-      <View>
-        <HomePresenter model={model} />
-      </View>
-    </ScrollView> 
+    <View style={{ flex: 1 }}>
+      <HomePresenter model={model} />
+    </View>
   );
 });

01a8b71ba22c7d7f5186c80c496c452bd4088701
WeiShenL <wslo@kth.se>
2026-06-03 11:58:32 +0100

    remove api keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,22 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// uh get .env file from mee.. or its alr in the grp chat
-
-// added for debug prupose
-console.log("ENV VARS:", {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-});
+// Config values are read from EXPO_PUBLIC_* env vars (see .env.example)
 
 export const firebaseConfig = {
-  // temporarily hard code
-  // apiKey: "AIzaSyA6R5uvR2hYh_u2rI4sS-Pn8IyKSfezIAs",
-  // authDomain: "solobuddy-4e91a.firebaseapp.com",
-  // projectId: "solobuddy-4e91a",
-
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
